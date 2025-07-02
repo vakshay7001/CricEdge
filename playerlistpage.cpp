02b928ed066dae68832e3702001ee1294664618f
vakshay7001 <157650221+vakshay7001@users.noreply.github.com>
2025-07-02 21:57:59 +0530

    Update playerlistpage.cpp

--- playerlistpage.cpp
+++ playerlistpage.cpp
@@ -9,22 +9,20 @@ PlayerListPage::PlayerListPage(QWidget *parent)
     , ui(new Ui::PlayerListPage)
 {
     ui->setupUi(this);
+   ui->playerTable->setColumnCount(6);
+     QStringList headers = { "Name", "Age", "Matches", "Runs", "Wickets", "Team" };
+       ui->playerTable->setHorizontalHeaderLabels(headers);
 
-    // Setup table headers
-    ui->playerTable->setColumnCount(6);
-    QStringList headers = { "Name", "Age", "Matches", "Runs", "Wickets", "Team" };
-    ui->playerTable->setHorizontalHeaderLabels(headers);
-
-    // Make the last section stretch and disable editing
+    
     ui->playerTable->horizontalHeader()->setStretchLastSection(true);
-    ui->playerTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+  ui->playerTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-    ui->playerTable->setColumnWidth(0, 250); // Name (more space)
-    ui->playerTable->setColumnWidth(1, 100);  // Age
-    ui->playerTable->setColumnWidth(2, 150);  // Matches
-    ui->playerTable->setColumnWidth(3, 180); // Runs
-    ui->playerTable->setColumnWidth(4, 100);  // Wickets
-    ui->playerTable->setColumnWidth(5, 100); // Team (last now)
+    ui->playerTable->setColumnWidth(0, 250); 
+    ui->playerTable->setColumnWidth(1, 100);  
+    ui->playerTable->setColumnWidth(2, 150);  
+    ui->playerTable->setColumnWidth(3, 180); 
+    ui->playerTable->setColumnWidth(4, 100);  
+    ui->playerTable->setColumnWidth(5, 100);
 
 }
 
@@ -45,14 +43,15 @@ void PlayerListPage::loadPlayers()
     for (int row = 0; row < players.size(); ++row) {
         const QStringList &fields = players[row];
 
-        ui->playerTable->setItem(row, 0, new QTableWidgetItem(fields[0])); // Name (bold below)
-        ui->playerTable->setItem(row, 1, new QTableWidgetItem(fields[1])); // Age
-        ui->playerTable->setItem(row, 2, new QTableWidgetItem(fields[3])); // Matches
-        ui->playerTable->setItem(row, 3, new QTableWidgetItem(fields[4])); // Runs
-        ui->playerTable->setItem(row, 4, new QTableWidgetItem(fields[5])); // Wickets
-        ui->playerTable->setItem(row, 5, new QTableWidgetItem(fields[2])); // Team
+        ui->playerTable->setItem(row, 0, new QTableWidgetItem(fields[0]));
+        
+  ui->playerTable->setItem(row, 1, new QTableWidgetItem(fields[1])); 
+  ui->playerTable->setItem(row, 2, new QTableWidgetItem(fields[3])); 
+     ui->playerTable->setItem(row, 3, new QTableWidgetItem(fields[4])); 
+    ui->playerTable->setItem(row, 4, new QTableWidgetItem(fields[5])); 
+        ui->playerTable->setItem(row, 5, new QTableWidgetItem(fields[2])); 
 
-        // Bold Name
+        
         QFont boldFont;
         boldFont.setBold(true);
         ui->playerTable->item(row, 0)->setFont(boldFont);
